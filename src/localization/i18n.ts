8d23d6f834f2fd08cdc4cc984da9fe459c7a8188
droptrigger <vasya.domakov@mail.ru>
2025-05-15 20:24:22 +0300

    Добавлена китайская локализация

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -3,11 +3,11 @@ import { I18n }from 'i18n-js';
 
 import en from './en';
 import ru from './ru';
-import ch from './ch';
+import zh from './zh';
 
-const i18n = new I18n({en, ru, ch})
+const i18n = new I18n({en, ru, zh})
 
-i18n.locale = 'ch';
+i18n.locale = Localization.locale || 'en';
 i18n.enableFallback = true;
 
 export default i18n;
